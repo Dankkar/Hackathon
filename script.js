3ed2a0938a4d0614e55d6802ec1f946013e1291b
Dankkar <cube71kd@gmail.com>
2024-04-13 19:32:00 +0700

    hoàn thành sub-menu

--- script.js
+++ script.js
@@ -1,28 +1,54 @@
 document.addEventListener('DOMContentLoaded', function () {
     var imageBoxes = document.querySelectorAll('.image-box');
+    var subMenuItems = document.querySelectorAll('.sub-menu li a');
   
-    imageBoxes.forEach(function (box) {
-      box.addEventListener('click', function () {
-        imageBoxes.forEach(function (otherBox) {
-            if (otherBox !== box) {
-              otherBox.remove();
-            }
-          });
-        var description = this.querySelector('.image-description').textContent;
-        
+    function displayContent(description) {
         var content = document.querySelector('#content');
         content.innerHTML = '<h1>' + description + '</h1><p>Đây là mô tả cho ' + description + '</p>';
-      });
-    });
-  });
-document.addEventListener('DOMContentLoaded', function () {
-  var homeButton = document.querySelector('#logo');
-  var homeLink = document.querySelector('#main-menu a[href="#"]');
+    }
+    function hideOtherBoxes(clickedBox) {
+        imageBoxes.forEach(function (box) {
+            if (box !== clickedBox) {
+                box.style.display = 'none';
+            } else {
+                box.style.display = 'block';
+            }
+        });
+    }
+    
 
-  [homeButton, homeLink].forEach(function (element) {
-    element.addEventListener('click', function (event) {
-      event.preventDefault();
-      location.reload();
+    function removeOtherBoxes(clickedBox) {
+        imageBoxes.forEach(function (box) {
+            if (box !== clickedBox) {
+                box.remove();
+            }
+        });
+    }
+  
+    imageBoxes.forEach(function (box) {
+        box.addEventListener('click', function () {
+            var description = this.querySelector('.image-description').textContent;
+            displayContent(description);
+            hideOtherBoxes(this);
+        });
     });
-  });
+    
+    subMenuItems.forEach(function (item, index) {
+        item.addEventListener('click', function (event) {
+            event.preventDefault();
+            var description = this.textContent;
+            displayContent(description);
+            hideOtherBoxes(imageBoxes[index]);
+        });
+    });
+
+    var homeButton = document.querySelector('#logo');
+    var homeLink = document.querySelector('#main-menu a[href="#"]');
+
+    [homeButton, homeLink].forEach(function (element) {
+        element.addEventListener('click', function (event) {
+            event.preventDefault();
+            location.reload();
+        });
+    });
 });
